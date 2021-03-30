e73d9a087f0baa19c3689ec82758f74a0875a4e9
Georgina Rojas <ginarojas29@gmail.com>
2021-03-30 15:06:49 -0300

    Were created API Routes on next js and was connected to DB Fauna.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,29 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import { FaGithub } from "react-icons/fa";
-import {FiX} from "react-icons/fi"
+import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  const [session] = useSession();
+
+  console.log("Session ", session);
+
+  return session ? (
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Georgina
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
