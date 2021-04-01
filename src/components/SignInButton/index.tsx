2c3f0b8467ffee09897b440c42c4b53252646ed4
Georgina Rojas <ginarojas29@gmail.com>
2021-04-01 16:50:56 -0300

    Clearing the code.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,8 +6,6 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
   const [session] = useSession();
 
-  console.log("Session ", session);
-
   return session ? (
     <button
       className={styles.signInButton}
