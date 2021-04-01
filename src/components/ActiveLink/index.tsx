c83fe34d1b2b3a91d73ebb8e557e46e3e338373e
Georgina Rojas <ginarojas29@gmail.com>
2021-04-01 13:21:18 -0300

    Enabling the menu navigation.

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/dist/client/router";
+import Link, {LinkProps} from "next/link";
+import { ReactElement, cloneElement } from "react";
+
+// ActiveLinkProps inherit all the props from Link
+interface ActiveLinkProps extends LinkProps {
+    children: ReactElement;
+    activeClassName: string;
+}
+
+export function ActiveLink({children, activeClassName, ...rest}: ActiveLinkProps){
+    const { asPath } = useRouter()
+
+    const className = asPath === rest.href ? activeClassName : "";
+
+    return(
+        <Link {...rest}>
+            {/* cloneElement allow to modify the properties of children */}
+            {cloneElement(children, {
+                className,
+            })}
+        </Link>
+    );
+}
